71c9e25b3e0f25fc7efaaf09dece9d8d5a88ed89
Cody <cody.tian@icloud.com>
2021-11-08 10:56:18 -0800

    version comments

---
+++ sketch.js
@@ -0,0 +1,26 @@
+/*
+@author Cody
+@date 2021-11-8
+
+version comments
+    sound wave with lifetime and radius
+    opacity and basic speaker with emitted sound waves
+    coloring
+
+ */
+let font
+
+function preload() {
+    font = loadFont('data/Meiryo-01.ttf')
+}
+
+function setup() {
+    createCanvas(640, 360)
+    colorMode(HSB, 360, 100, 100, 100)
+}
+
+function draw() {
+    background(209, 80, 30)
+    
+    background(234, 34, 24)
+}
