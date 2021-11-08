3430027055a304c7bdba51bad7ec7cce6e764844
Cody <cody.tian@icloud.com>
2021-11-08 11:10:15 -0800

    add soundWave .update() and .show()

--- sketch.js
+++ sketch.js
@@ -3,12 +3,13 @@
 @date 2021-11-8
 
 version comments
-    sound wave with lifetime and radius
-    opacity and basic speaker with emitted sound waves
+    sound wave with lifetime and radius, opacity
+    basic speaker with emitted sound waves
     coloring
 
  */
 let font
+let soundWave
 
 function preload() {
     font = loadFont('data/Meiryo-01.ttf')
@@ -17,10 +18,31 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+    soundWave = new SoundWave(random(width), random(height))
 }
 
 function draw() {
-    background(209, 80, 30)
+    // background(209, 80, 30)
     
     background(234, 34, 24)
+
+    soundWave.show()
+    soundWave.update()
+}
+
+class SoundWave {
+    constructor(x, y) {
+        this.pos = new p5.Vector(x, y)
+        this.lifetime = 100
+    }
+
+    show() {
+        noFill()
+        stroke(0, 0, 100, this.lifetime)
+        circle(this.pos.x, this.pos.y, (100-this.lifetime)*width/75)
+    }
+
+    update() {
+        this.lifetime -= 1
+    }
 }
